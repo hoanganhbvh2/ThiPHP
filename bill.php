51e6d7324d1ba4e5d134e31c5236a4975ae21650
Nguyen hoang Anh <hoanganhbvh2@gmail.com>
2025-06-16 15:18:55 +0700

    update

---
+++ bill.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bill</title>
+</head>
+
+<body>
+    <?php
+// Trường 
+// id int
+// customer string
+// usage int
+// month string
+// rate int
+    $bill=[
+    ["id"=>1,"customer" =>"client A","usage"=>30,"month"=>"6/6/2025","rate"=>3000],
+    ["id"=>2,"customer" =>"client b","usage"=>20,"month"=>"6/6/2025","rate"=>3000],
+    ["id"=>3,"customer" =>"client C","usage"=>40,"month"=>"7/6/2025","rate"=>4000]
+    ];   
+        ?>
+        <h2>Bill thanh toan </h2>
+        <a href="timkiem.php">Tim kiem </a>
+    
+    
+    <table border=1>
+        <tr>
+            <th>id</th>
+            <th>customer</th>
+            <th>Usage</th>
+            <th>month</th>
+            <th>rate</th>
+            <th>total</th>
+            <th>total1</th>
+        </tr>
+        <?php foreach($bill as $value){ ?>
+        <tr>
+            <td><?php echo  $value["id"];?></td>
+            <td><?php echo  $value["customer"];?></td>
+            <td><?php echo  $value["usage"];?></td>
+            <td><?php echo  $value["month"];?></td>
+            <td><?php echo  $value["rate"];?></td>
+            <?php
+            $total=$value["usage"]*$value["rate"];
+            $total1=$total*(1-test($value["usage"]))
+            ?>
+            <td><?php echo $total ;?></td>
+            <td><?php echo $total1 ;?></td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <?php
+    function test($u){
+        if($u>=300){
+            return 0.2;
+        }
+        elseif($u<300&&$u>=100){
+            return 0.1;
+        }
+        else{
+            return 0.05;
+        }
+    }
+    ?>
+
+
+
+</body>
+
+</html>
---
+++ timkiem.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tim kiem</title>
+</head>
+
+<body>
+    <?php
+        // $bill=[
+        // ["id"=>1,"customer" =>"client A","usage"=>30,"month"=>"6/6/2025","rate"=>3000],
+        // ["id"=>2,"customer" =>"client b","usage"=>20,"month"=>"6/6/2025","rate"=>3000],
+        // ["id"=>3,"customer" =>"client C","usage"=>40,"month"=>"7/6/2025","rate"=>4000]
+        // ];
+    // truong hop 1: neu khong co dau hieu tk ==> toan bo danh sach
+    // truong hop 2: neu co tk , so sanh cai tim kiem do=> danh sach moi==> table
+
+
+
+    ?>
+    
+
+
+
+
+
+    <?php
+    if(!isset($_GET["tk"]))  {
+          $bill=[
+    ["id"=>1,"customer" =>"client A","usage"=>30,"month"=>"6/6/2025","rate"=>3000],
+    ["id"=>2,"customer" =>"client b","usage"=>20,"month"=>"6/6/2025","rate"=>3000],
+    ["id"=>3,"customer" =>"client C","usage"=>40,"month"=>"7/6/2025","rate"=>4000]
+          ];
+    ?>
+    //table in tat ca ket qua
+    <table border=1>
+        <tr>
+            <th>id</th>
+            <th>customer</th>
+            <th>Usage</th>
+            <th>month</th>
+            <th>rate</th>
+        </tr>
+        <?php foreach($bill as $value){ ?>
+        <tr>
+            <td><?php echo  $value["id"];?></td>
+            <td><?php echo  $value["customer"];?></td>
+            <td><?php echo  $value["usage"];?></td>
+            <td><?php echo  $value["month"];?></td>
+            <td><?php echo  $value["rate"];?></td>
+
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <?php
+    }else{
+    $bill=[
+    ["id"=>1,"customer" =>"client A","usage"=>30,"month"=>"6/6/2025","rate"=>3000],
+    ["id"=>2,"customer" =>"client b","usage"=>20,"month"=>"6/6/2025","rate"=>3000],
+    ["id"=>3,"customer" =>"client C","usage"=>40,"month"=>"7/6/2025","rate"=>4000]
+          ];
+     $bill1=[];     
+        foreach($bill as $v){
+            if($_GET["tk"]==$v["customer"]||$_GET["tk"]==$v["month"]){
+                array_push($bill1,$v);
+            }
+        }
+        ?>
+    // in ra ket qua da duoc tim kiem
+    <table border=1>
+        <tr>
+            <th>id</th>
+            <th>customer</th>
+            <th>Usage</th>
+            <th>month</th>
+            <th>rate</th>
+        </tr>
+        <?php foreach($bill1 as $value){ ?>
+        <tr>
+            <td><?php echo  $value["id"];?></td>
+            <td><?php echo  $value["customer"];?></td>
+            <td><?php echo  $value["usage"];?></td>
+            <td><?php echo  $value["month"];?></td>
+            <td><?php echo  $value["rate"];?></td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <?php
+    }
+    ?>
+    <form action="timkiem.php" medthod="get">
+        <input type="text" name="tk">
+        <input type="submit" value="Tim kiem">
+    </form>
+
+</body>
+
+</html>
